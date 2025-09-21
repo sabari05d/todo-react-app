dc83190d453d2b4dd81d5cac126a1cd4e6c035af
Sabarinathan D <nsabarinathan.in@gmail.com>
2025-09-21 17:01:42 +0530

    Fixed PWA All

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
-import icon192 from './src/assets/logo/icon-192x192.png'
-import icon512 from './src/assets/logo/icon-512x512.png'
 
 export default {
   plugins: [
@@ -16,8 +14,16 @@ export default {
         background_color: '#ffffff',
         theme_color: '#4a90e2',
         icons: [
-          { src: icon192, sizes: '192x192', type: 'image/png' },
-          { src: icon512, sizes: '512x512', type: 'image/png' }
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
         ]
       }
     })

516c44a0159581dbc5433af625c521dcdd265969
Sabarinathan D <nsabarinathan.in@gmail.com>
2025-10-01 20:41:14 +0530

    Backend Finished and URL Updated

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://127.0.0.1:8000/api",
-    baseURL: "https://doquest.infinityfreeapp.com/api",
+    // baseURL: "https://doquest.infinityfreeapp.com/api",
+    baseURL: "https://cors-anywhere.herokuapp.com/https://doquest.infinityfreeapp.com/api",
     headers: {
         "Content-Type": "application/json",
     },
